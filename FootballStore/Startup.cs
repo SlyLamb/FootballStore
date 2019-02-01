210d7ea23f929e149ed0c352187e674b439fbed3
Shelton Lai Lam <shelton.lailam@gmail.com>
2019-02-01 14:57:47 +1300

    setup vs

---
+++ FootballStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FootballStore.Startup))]
+namespace FootballStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
