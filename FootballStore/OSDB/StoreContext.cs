81eb2788ef2b9929b3e5b91a16f5d244c22f5a57
Shelton Lai Lam <shelton.lailam@gmail.com>
2019-02-04 11:49:01 +1300

    created db context, product andcategory classes

---
+++ FootballStore/OSDB/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FootballStore.Models;
+using System.Data.Entity;
+
+namespace FootballStore.OSDB
+{
+    public class StoreContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
