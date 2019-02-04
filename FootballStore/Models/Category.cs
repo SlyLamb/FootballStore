3505dff859df5745be8e488ea374044c2159c96f
Shelton Lai Lam <shelton.lailam@gmail.com>
2019-02-04 13:45:36 +1300

    added data annotations for product and category

--- FootballStore/Models/Category.cs
+++ FootballStore/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,10 @@ namespace FootballStore.Models
     {
         public int ID { get; set; }
 
+        [Display(Name = "Category Name")]
+        [Required(ErrorMessage = "The category name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a category name between 3 and 50 characters in length")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Please enter a category name beginning with a capital letter and only enter letters and spaces")]
         public string Name { get; set; }
 
         public virtual ICollection<Product> Products { get; set; }
--- FootballStore/Models/Product.cs
+++ FootballStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,24 @@ namespace FootballStore.Models
 {
     public class Product
     {
+
         public int ID { get; set; }
 
+        [Required(ErrorMessage = "The Product name cannot be blank")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Please enter a product name between 3 and 50 characters in length")]
+        [RegularExpression(@"^[a-zA-Z'_'\s]*$", ErrorMessage = "Please enter a product name made up of letters and spaces")]
+        [Display(Name = "Product Name")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "The Product description cannot be blank")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Please enter a product description between 10 and 200 characters in length")]
+        [RegularExpression(@"^[a-zA-Z'_'\s]*[0-9]*$", ErrorMessage = "Please enter a product description made up of only letters and spaces")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "The Product price cannot be blank")]
+        [Range(0.10, 10000, ErrorMessage = "Please enter a product price between 0.10 and 10000")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:c}")]
         public decimal Price { get; set; }
 
         public int? CategoryID { get; set; }
