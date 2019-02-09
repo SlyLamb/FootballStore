264752bd0e49fc05338db178ab508eb60969c75f
gabrieldpg <gabrielpcpg@gmail.com>
2019-02-09 23:34:25 +1300

    added styling

--- FootballStore/App_Start/BundleConfig.cs
+++ FootballStore/App_Start/BundleConfig.cs
@@ -25,6 +25,11 @@ namespace FootballStore
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/reset.css",
+                      "~/Content/bootstrap.css",
+                      "~/Content/ofutebol.css"));
         }
     }
 }
